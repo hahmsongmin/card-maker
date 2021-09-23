6cfa140b9ac311baca959b0330b19816a5d2668a
hahmsongmin <78192018+hahmsongmin@users.noreply.github.com>
2021-09-23 20:48:11 +0900

    add Maker Preview layout

---
+++ src/components/editor/editor.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./edit.module.css";
+
+const Editor = () => {
+  return (
+    <section className={styles.editor}>
+      <div className={styles.title}>Card Maker</div>
+    </section>
+  );
+};
+
+export default Editor;
