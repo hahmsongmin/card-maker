8ef819170b2881ed174af926343141d8b0770c01
hahmsongmin <78192018+hahmsongmin@users.noreply.github.com>
2021-09-27 10:50:48 +0900

    firebase realtime database

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -6,52 +6,32 @@ import Header from "../header/header";
 import Preview from "../preview/preview";
 import styles from "./maker.module.css";
 
-const Maker = ({ FileInput, authService }) => {
-  const [cards, setCards] = useState({
-    1: {
-      id: "1",
-      name: "songmin",
-      company: "google",
-      theme: "light",
-      title: "software engineer",
-      email: "tft0720@gmail.com",
-      message: "go for it",
-      fileName: null,
-      fileURL: null,
-    },
-    2: {
-      id: "2",
-      name: "songmin2",
-      company: "google",
-      theme: "dark",
-      title: "software engineer",
-      email: "tft0720@gmail.com",
-      message: "go for it",
-      fileName: null,
-      fileURL: null,
-    },
-    3: {
-      id: "3",
-      name: "songmin3",
-      company: "google",
-      theme: "colorful",
-      title: "software engineer",
-      email: "tft0720@gmail.com",
-      message: "go for it",
-      fileName: null,
-      fileURL: null,
-    },
-  });
-
+const Maker = ({ FileInput, authService, cardRepository }) => {
   const history = useHistory();
+  const historyState = history.location?.state;
+  const [cards, setCards] = useState({});
+  const [userId, setUserId] = useState(historyState && historyState.id);
 
   const onLogout = () => {
     authService.logout();
   };
 
+  // useEffect 에서 어떠한 함수를 리턴하면 리액트가 알아서 unmount 되었을때 실행해 줌
+  useEffect(() => {
+    if (!userId) {
+      return;
+    }
+    const stopSync = cardRepository.syncCards(userId, (cards) => {
+      setCards(cards);
+    });
+    return () => stopSync();
+  }, [userId]);
+
   useEffect(() => {
     authService.onAuthChange((user) => {
-      if (!user) {
+      if (user) {
+        setUserId(user.uid);
+      } else {
         history.push("/");
       }
     });
@@ -63,6 +43,7 @@ const Maker = ({ FileInput, authService }) => {
       updated[card.id] = card;
       return updated;
     });
+    cardRepository.saveCard(userId, card);
   };
   const deleteCard = (card) => {
     setCards((cards) => {
@@ -70,6 +51,7 @@ const Maker = ({ FileInput, authService }) => {
       delete updated[card.id];
       return updated;
     });
+    cardRepository.removeCard(userId, card);
   };
 
   return (
